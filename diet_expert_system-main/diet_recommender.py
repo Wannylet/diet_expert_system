47952b6091be34f2c1e4364e8806182eadf63ea9
José Hernández Carrera <97087418+Wannylet@users.noreply.github.com>
2025-09-30 10:00:00 -0600

    Delete diet_expert_system-main directory

--- diet_expert_system-main/diet_recommender.py
+++
@@ -1,8 +0,0 @@
-# diet_recommender.py
-from typing import Dict, Any
-import pandas as pd
-from rules import get_diet_plan
-
-def recommend_diet(client_rules: pd.DataFrame) -> Dict[str, Any]:
-    client_rules = client_rules.iloc[0].to_dict()
-    return get_diet_plan(client_rules['diet_type'], client_rules['goal'])
